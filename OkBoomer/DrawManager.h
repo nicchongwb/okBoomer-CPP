e5ba8113a349801759d3e24c1f634ee0fe0dabb2
BestCrochet <alicia159@live.com.sg>
2021-03-16 19:40:24 +0800

    drawManager (wip)

---
+++ OkBoomer/DrawManager.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef DRAWMANAGER_H
+#define DRAWMANAGER_H
+
+#include "Vector2D.h"
+
+class DrawManager {
+
+public:
+	DrawManager() {
+
+	}
+
+};
+#endif
--- OkBoomer/Vector2D.h
+++ OkBoomer/Vector2D.h
@@ -11,10 +11,10 @@
 
 class Vector2D {
 public:
-    float X, Y;
+    int X, Y;
 
 public:
-    Vector2D(float x = 0, float y = 0) : X(x), Y(y) {}
+    Vector2D(int x = 0, int y = 0) : X(x), Y(y) {}
 
 public:
     // addition +
@@ -25,11 +25,6 @@ public:
     // difference -
     inline Vector2D operator-(const Vector2D& v2) const {
         return Vector2D(X - v2.X, Y - v2.Y);
-    }
-
-    // multiplication *
-    inline Vector2D operator*(const float scalar) const {
-        return Vector2D(X * scalar, Y * scalar);
     }
 
     void Log(std::string msg = "") {
